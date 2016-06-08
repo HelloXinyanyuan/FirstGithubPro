76a5d995514ae42c8d71309db3dfb62ba9aeb2dc
abc2016qwe <abc2016qwe@sohu.com>
2016-06-08 18:24:58 +0800

    commit again

--- src/com/whunf/Tdfsadfa.java
+++ src/com/whunf/Tdfsadfa.java
@@ -1,9 +1,13 @@
 package com.whunf;
+
 /**
  * 我的说法就是多了
+ * 
  * @author Administrator
  *
  */
 public class Tdfsadfa {
-
+	public static void main(String[] args) {
+		System.out.println("Tdfsadfa.main()");
+	}
 }
